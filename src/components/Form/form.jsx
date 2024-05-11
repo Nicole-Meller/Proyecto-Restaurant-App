7823c9ce5fa1c25a87bf257fdd9af45c3613a8f2
Nicole Meller <meller.nicole@gmail.com>
2024-05-11 17:06:12 -0400

    fix

--- src/components/Form/form.jsx
+++ src/components/Form/form.jsx
@@ -7,7 +7,7 @@ return(
             <div className="-mx-3 flex flex-wrap">
                 <div className="w-full px-3 sm:w-1/2">
                     <div className="mb-5">
-                        <label for="name" className="mb-3 block text-base font-medium text-[#3a4d07]">
+                        <label for="name" className="mb-3 block text-base font-medium text-[#07074D]">
                             Nombres
                         </label>
                         <input type="text" value={names}
@@ -16,7 +16,7 @@ return(
                         className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
                     </div>
                 </div>
-                <div className="w-full px-3 sm:w-1/2">
+                <div classNamess="w-full px-3 sm:w-1/2">
                     <div className="mb-5">
                         <label for="name" className="mb-3 block text-base font-medium text-[#07074D]">
                             Apellidos
