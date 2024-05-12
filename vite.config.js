da471abe2663ad5110f466ca160ca5b3cc59d421
Nicole Meller <meller.nicole@gmail.com>
2024-05-12 14:14:21 -0400

    vite config

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default {
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom']
-    }
-  }
-}
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/nicole-meller.github.io"
+})
