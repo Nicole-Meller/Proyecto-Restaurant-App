e821b264d4f95d6b3cb54a8a99773be97e33303a
Nicole Meller <meller.nicole@gmail.com>
2024-05-12 13:07:57 -0400

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { BrowserRouter} from 'react-router-dom'
 import React from 'react';
+import { BrowserRouter} from 'react-router-dom'
 import { Header } from './components/Header/Header'
 import { Footer } from './components/Footer/Footer'
 import { RouterApp } from '../routes/RouterApps'
